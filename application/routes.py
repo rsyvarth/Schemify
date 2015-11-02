594978c5dbfd6d3e9f2caa36ebb2edf4003ca884
Robert Syvarth <robert@duxter.com>
2015-11-02 11:09:49 -0600

    Complete file upload/hosting

--- application/routes.py
+++ application/routes.py
@@ -12,6 +12,7 @@ from application import app
 
 from controllers.AuthController import login, logout
 from controllers.PaletteController import Palette, PaletteList
+from controllers.ImageController import Image, ImageList
 from controllers.UserController import UserSelf
 
 # Allow cross domain requests from localhost 
@@ -19,9 +20,13 @@ CORS(app, resources={r"/api/*": {"origins": "http://localhost:1234"}})
 
 # API Endpoints
 api = Api(app)
+api.add_resource(UserSelf, '/api/users/self')
+
 api.add_resource(PaletteList, '/api/palettes')
 api.add_resource(Palette, '/api/palettes/<entry_id>')
-api.add_resource(UserSelf, '/api/users/self')
+
+api.add_resource(ImageList, '/api/images')
+api.add_resource(Image, '/api/images/<image_id>')
 
 # Login page
 app.add_url_rule('/login', 'login', view_func=login)
